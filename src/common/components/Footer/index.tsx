c865d6c2256429542bace635185b86c2d4a53fec
jelly771001 <jelly771001@gmail.com>
2024-01-14 08:33:20 +0800

    updata index

--- src/common/components/Footer/index.tsx
+++ src/common/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { grassMotionSet } from "@/constants/globaIIcon";
-import { icons } from "./data";
 import LogoImg from "../LogoImg";
+import { icons } from "./data";
 
 const Footer = () => {
   const { grassMotionLeft, grassMotionRight } = grassMotionSet
