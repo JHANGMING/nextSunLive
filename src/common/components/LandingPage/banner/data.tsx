6b5313b28cf497ee6c731177c460be1366074002
jelly771001 <jelly771001@gmail.com>
2024-01-15 09:54:49 +0800

    updata banner

--- src/common/components/LandingPage/banner/data.tsx
+++ src/common/components/LandingPage/banner/data.tsx
@@ -2,32 +2,27 @@ export const bannerImgData = [
   {
     src: '/images/home/banner/banner1.png',
     alt: 'banner1',
-    width: 1440,
-    height: 600,
+
   },
   {
     src: '/images/home/banner/banner2.png',
     alt: 'banner2',
-    width: 1440,
-    height: 600,
+
   },
   {
     src: '/images/home/banner/banner3.png',
     alt: 'banner3',
-    width: 1440,
-    height: 600,
+
   },
   {
     src: '/images/home/banner/banner4.png',
     alt: 'banner4',
-    width: 1440,
-    height: 600,
+
   },
   {
     src: '/images/home/banner/banner5.png',
     alt: 'banner5',
-    width: 1440,
-    height: 600,
+
   },
 ]
 
