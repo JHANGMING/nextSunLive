0e3ced3419a6300e957b8452db4401898e2b6da4
jelly771001 <jelly771001@gmail.com>
2024-01-18 10:02:57 +0800

    updata index

--- src/modules/ProductPage/CategoryTitle.tsx
+++ src/modules/ProductPage/CategoryTitle.tsx
@@ -2,13 +2,13 @@ import LogoImg from "@/common/components/Logo/LogoImg";
 type CategoryTitleProps = {
   title:string
 };
-const CategoryTitle = ({title}) => {
+const CategoryTitle = ({ title }: CategoryTitleProps) => {
   return (
     <div className="flex gap-10 p-16 h-74">
       <LogoImg widthProps={32} heightProps={32} />
       <h3 className="ellipse">{title}</h3>
     </div>
   );
-}
+};
  
 export default CategoryTitle;
