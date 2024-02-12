de98735b3dba5c94a6c4b1ee1a5ee7fe0922de58
jelly771001 <jelly771001@gmail.com>
2024-02-12 15:30:34 +0800

    Add LiveBanner and LiveChat components

--- src/modules/LiveStreamView/index.tsx
+++ src/modules/LiveStreamView/index.tsx
@@ -1,13 +1,16 @@
 import ViewBanner from '@/common/components/LiveStreamPage/ViewBanner';
 import LiveViewer from './LiveViewer';
 import LiveSaleSection from './LiveSaleSection';
+import LiveBanner from '@/common/components/LiveStreamPage/LiveBanner';
+import LiveChat from './LiveChat';
 
 const LiveStreamView = () => {
   return (
     <>
-      <ViewBanner />
+      {/* <ViewBanner />
       <LiveViewer />
-      <LiveSaleSection />
+      <LiveSaleSection /> */}
+      <LiveChat />
     </>
   );
 };
