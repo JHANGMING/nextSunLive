5d17493d9b91444cad2b3050f2f93bf5bee6b128
jelly771001 <jelly771001@gmail.com>
2024-02-15 11:17:28 +0800

    Update assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '4.224.41.94',
-      },
-    ],
-  },
+  assetPrefix: 'https://4.224.41.94',
 };
 
 module.exports = nextConfig;
