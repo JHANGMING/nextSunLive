b3ed41218bb613fcf8287c70c6f3cfae9e90836c
jelly771001 <jelly771001@gmail.com>
2024-02-15 11:02:57 +0800

    Add SignalR dependencies and implement live chat functionality

--- src/modules/LiveStreamView/LiveChat.tsx
+++ src/modules/LiveStreamView/LiveChat.tsx
@@ -1,33 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import GlobalLink from '@/common/components/GlobalLink';
 import { BsCursorFill } from 'react-icons/bs';
-
-
+type Message = {
+  sender?: string;
+  message: string;
+};
 const LiveChat = () => {
   const [chatroomId, setChatroomId] = useState(null);
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState('');
-  const [userIdSender, setUserIdSender] = useState(3);
-
+  const [userIdSender, setUserIdSender] = useState(6);
+  const [isConnected, setIsConnected] = useState(false); 
+  const chatHubProxyRef = useRef<SignalR.Hub.Proxy | null>(null); 
+  const messagesEndRef = useRef<HTMLUListElement | null>(null);
+  const token =
+    'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6NiwiVXNlckNhdGVnb3J5IjowLCJJYXQiOiJcL0RhdGUoMTcwNzg5MTE2ODY5MylcLyIsIkV4cCI6IjIvMTUvMjAyNCAyOjEyOjQ4IFBNIn0.aAIr2GBb8HoO_moIuRNjnKQJQAukUTQELrHyDnO10nIiBUrwO2nC--S-R-A2xpwD6UG5eNz3Kbudyvkukdxowg';
+   useEffect(() => {
+     if (messagesEndRef.current) {
+       const { current: messagesContainer } = messagesEndRef;
+       messagesContainer.scrollTop = messagesContainer.scrollHeight;
+     }
+   }, [messages]);
   useEffect(() => {
     if (typeof window !== 'undefined') {
       import('signalr-no-jquery').then(({ hubConnection }) => {
         const connection = hubConnection('http://4.224.41.94');
-        const chatHubProxy = connection.createHubProxy('chathub');
+        const chatHubProxy = connection.createHubProxy('chathub')as unknown as SignalR.Hub.Proxy;
+        chatHubProxy.on('receiveMessage', (message) => {
 
-        chatHubProxy.on('receiveMessage', (sender, message) => {
-          const newMessage = { sender, message };
-          setMessages((prevMessages) => [...prevMessages, newMessage]);
+          console.log('Received message:', message);
+          setMessages(message.chatcontent);
+          // 在這裡處理收到的消息
         });
-
+         chatHubProxyRef.current = chatHubProxy;
         connection
           .start()
           .done(() => {
             console.log('Connected to SignalR server!');
+            setIsConnected(true);
+            callApi();
+            if (chatHubProxyRef.current) {
+              chatHubProxyRef.current
+                .invoke('Hello') // 假设Hello方法不需要参数，或者传递 null/undefined 作为空参数
+                .then((res) => {
+                  console.log('Response from Hello method:', res);
+                });
+            }
           })
-          .fail((error) => {
-            console.error('Failed to connect to SignalR server: ', error);
+          .fail((error: Error) => {
+            console.log(error);
+            // console.error('Failed to connect to SignalR server: ', error);
           });
 
         // 断开连接的逻辑也应该在这里
@@ -37,23 +60,74 @@ const LiveChat = () => {
       });
     }
   }, []);
+  const callApi = () => {
+    // 使用 fetch 发送 POST 请求
+    fetch('http://4.224.41.94/api/chats/joinroom/6/8', {
+      method: 'POST', // 指定请求方法为 POST
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        console.log('API called successfully:', response);
 
-  // const handleSendMessage = () => {
-  //    const connection = hubConnection('http://4.224.41.94');
-  //   const chatHubProxy = connection.createHubProxy('chathub'); // 重新获取proxy，确保其在当前作用域内有效
-  //   // 发送消息
-  //   if (chatroomId && newMessage.trim() !== '') {
-  //     chatHubProxy
-  //       .invoke('SendMessageToRoom', chatroomId, userIdSender, newMessage)
-  //       .done(() => {
-  //         console.log('Message sent successfully');
-  //         setNewMessage(''); // 清空输入框
-  //       })
-  //       .fail((error) => {
-  //         console.error('Failed to send message: ', error);
-  //       });
-  //   }
-  // };
+        return response.json(); // 解析 JSON 数据
+      })
+      .then((response) => {
+        console.log('API called successfully:', response);
+        // 在这里处理 API 调用成功的逻辑
+        setMessages(response.chatcontent);
+        const { chatroomId } = response;
+        setChatroomId(chatroomId); // api会返回chatroomId，
+        
+        let userIdSender = 6; // 这里我先手动设置参数userIdSender=5
+        let message = 'message test for 11@11.com user'; // 这里我先手动设置参数message string
+
+        // 调用下面的方法
+        JoinChatRoom(chatroomId); // 先加入2人专属的聊天室
+        // SendMessageToRoom(chatroomId, userIdSender, message); // 之后通过这个function发送消息
+      })
+      .catch((error) => {
+        console.error('Failed to call API:', error);
+        // 在这里处理 API 调用失败的逻辑
+      });
+  };
+    const JoinChatRoom = (chatroomId:number) => {
+      const chatHubProxy = chatHubProxyRef.current;
+    if (chatHubProxy) {
+      chatHubProxy.invoke('JoinChatRoom', chatroomId)
+        .done((response) => {
+          console.log('Joined chat room successfully:', response);
+          
+        })
+        .fail((error) => {
+          // 失败逻辑...
+        });
+    } else {
+      console.error('ChatHubProxy is not initialized.');
+    }
+    };
+const handleSendMessage = () => {
+  if (!isConnected) {
+    console.error('SignalR connection is not established.');
+    return;
+  }
+
+  if (chatroomId && newMessage.trim() !== '') {
+    // console.log('Sending message...', newMessage);
+    const chatHubProxy = chatHubProxyRef.current;
+    chatHubProxy
+      ?.invoke('SendMessageToRoom', chatroomId, userIdSender, newMessage)
+      .done(() => {
+        console.log('Message sent successfully');
+        setNewMessage(''); // 清空输入框
+      })
+      .fail((error) => {
+        console.error('Failed to send message: ', error);
+      });
+  }
+};
 
   return (
     <div className="mt-[250px] mb-40 mx-auto w-[600px] border border-lightGray rounded-16">
@@ -92,11 +166,13 @@ const LiveChat = () => {
             </GlobalLink>
           </div>
         </div>
-        <ul className="flex flex-col gap-16">
-          {messages.map((msg, index) => (
+        <ul
+          className="flex flex-col gap-16 overflow-y-auto max-h-[400px]"
+          ref={messagesEndRef}>
+          {messages?.map((msg, index) => (
             <li key={index} className="flex items-center gap-16">
-              <span>{msg.sender}</span>
-              <p className="text-14">{msg.message}</p>
+              {/* <span>{msg.sender}</span> */}
+              <p className="text-14">{msg?.message}</p>
             </li>
           ))}
         </ul>
@@ -116,11 +192,11 @@ const LiveChat = () => {
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
         />
-        {/* <BsCursorFill
+        <BsCursorFill
           size={24}
           onClick={handleSendMessage}
           className=" text-primary-red cursor-pointer hover:opacity-60"
-        /> */}
+        />
       </div>
     </div>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <script src="/jquery-1.6.4.js"></script>
+        <script src="/jquery.signalR-2.4.3.js"></script>
+      </Head>
       <Head />
       <body>
         <Main />
