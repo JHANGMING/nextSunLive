fd4c7f2aeecc80f0ae6530dc191a5e43fc72212d
jelly771001 <jelly771001@gmail.com>
2024-02-15 11:45:58 +0800

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
